592556a70114b595a37551d6970cea728766f803
William Lee <leewp8@gmail.com>
2022-04-22 13:56:06 -0400

    server set up and migration

---
+++ data/migrations/20220422162448_create_tables.js
@@ -0,0 +1,47 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+        tbl.increments('project_id')
+        tbl.string('project_name', 100).notNullable()
+        tbl.string('project_description', 200)
+        tbl.boolean('project_completed').notNullable().defaultTo(0)
+    })
+    .createTable('resources', tbl => {
+        tbl.increments('resource_id')
+        tbl.string('resource_name', 100).notNullable().unique()
+        tbl.string('resource_description', 200)
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments('task_id')
+        tbl.string('task_description', 100).notNullable()
+        tbl.string('task_notes', 200)
+        tbl.boolean('task_completed').notNullable().defaultTo(0)
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+    })
+    .createTable('project_resources', tbl => {
+        tbl.increments('project_resource_id')
+        tbl.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+        tbl.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+    })
+};
+
+exports.down = function(knex) {
+    knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
